66d700af27c23bfeb8477fcd941297952bd12210
Sy Rashid <sy.rashid.11@gmail.com>
2021-11-05 19:04:24 +0000

    FEAT: Set up first successful endpoint"

--- twitter_services/user_lookup.rb
+++ twitter_services/user_lookup.rb
@@ -9,14 +9,11 @@ module TwitterServices
         url = "https://api.twitter.com/2/users/by/username/#{@username}"
         headers = {
           'Accept'=> 'application/json',
-          'Content-Type'=> 'application/json',
           'Authorization'=> "Bearer #{ENV['TWITTER_BEARER_TOKEN']}"
         }
-        binding.pry
-        response = Faraday.get(url, headers)
 
+        response = Faraday.get(url, nil, headers)
         OpenStruct.new({success?: response.status == 200, payload: response})
-
       rescue => e
         puts e.message
       end
